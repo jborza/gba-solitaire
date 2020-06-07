d7fe81c2762401e29223506a9a4d17e6a64d0d94
jborza <jborza@gmail.com>
2020-06-07 11:05:39 +0200

    palette source

---
+++ source/palette.c
@@ -0,0 +1,20 @@
+#include "color.h"
+
+void initialize_palette(){
+palette[0] = 0x0;
+palette[1] = 0x7fff;
+palette[2] = 0x1f;
+palette[3] = 0x3e0;
+palette[4] = 0x7c00;
+palette[5] = 0x21e0;
+palette[6] = 0x7ee6;
+palette[7] = 0x5d63;
+palette[8] = 0x2a3b;
+palette[9] = 0x10c;
+palette[10] = 0x39df;
+palette[11] = 0x18c6;
+palette[12] = 0x39ce;
+palette[13] = 0x33f5;
+palette[14] = 0x7e20;
+palette[15] = 0x5ef7;
+}
---
+++ source/palette.h
@@ -0,0 +1,12 @@
+int palette[16];
+
+#define BLACK 0
+#define WHITE 1
+#define RED 2
+#define GREEN 3
+#define BLUE 4
+#define DARKGREEN 5
+#define LIGHTBLUE 6
+#define DARKBLUE 7
+
+void initialize_palette();
